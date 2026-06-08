80708480b59082a7be1b2bf9e6f317195a107e8e
news structure <maisonatelier52@gmail.com>
2026-06-08 16:11:00 +0530

    remove the commands

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,3 @@
-
-
-// // models/Category.js
-// const mongoose = require('mongoose');
-
-// const categorySchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true, unique: true },
-//     slug: { type: String, required: true, unique: true },
-//     bannerImage: { type: String, default: '' }, // New field for banner image
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model('Category', categorySchema);   
-
 // models/Category.js
 
 const mongoose = require('mongoose');
